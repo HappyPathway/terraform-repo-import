3303fc976c7664d2fef68417e5331032a1cbe8b8
Dave Arnold <dave@roknsound.com>
2024-08-15 12:09:03 -0700

    cleaning up stuff

--- import.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/HappyPathway/terraform-importer-gh-actions.git ../terraform-import-gh-actions-internal || { cd ../terraform-import-gh-actions-internal && git pull origin main; }
-cd ../terraform-import-gh-actions-internal
-git fetch
-git remote add internal git@github.com:HappyPathway/terraform-import-gh-actions-internal.git
-git push internal main --force
-git push --tags internal
-cd ..
